85701492347db06f8f41d8b4b77d2d4b5e362777
usfalidiate <51782274+usfalidiate@users.noreply.github.com>
2024-12-06 08:20:59 +1100

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,21 @@
+// firebase-config.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-auth.js";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC04QzoczMLF4p3qa1WjrcnKzke-pI5SRU",
+  authDomain: "gamedlessons.firebaseapp.com",
+  projectId: "gamedlessons",
+  storageBucket: "gamedlessons.firebasestorage.app",
+  messagingSenderId: "346571176421",
+  appId: "1:346571176421:web:7d5a0e2bffb096fa40970c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+export { db, auth };
